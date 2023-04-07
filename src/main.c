c06701ddabd24e35912c8c713dd85a980e3ad32a
Chyi-Kwei Yau <chyikwei.yau@gmail.com>
2023-04-06 23:08:33 -0700

    add debug file

--- src/main.c
+++ src/main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "common.h"
 #include "chunk.h"
+#include "debug.h"
 
 #ifndef TEST
 int main(int argc, const char* argv[]) {
   Chunk chunk;
 	initChunk(&chunk);
 	writeChunk(&chunk, OP_RETURN);
+
+	disassembleChunk(&chunk, "test chunk");
 	freeChunk(&chunk);
-	printf("test\n");
 	return 0;
 }
 #endif
