c08096eeb1dbf85a4c64965084e55383b6c59103
Chyi-Kwei Yau <chyikwei.yau@gmail.com>
2023-04-23 22:58:40 -0700

    add line number

--- src/main.c
+++ src/main.c
@@ -8,13 +8,13 @@ int main(int argc, const char* argv[]) {
   Chunk chunk;
 	initChunk(&chunk);
 	int constant = addConstant(&chunk, 1.2);
-	writeChunk(&chunk, OP_CONSTANT);
-	writeChunk(&chunk, constant);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constant, 123);
 	int constant2 = addConstant(&chunk, 1.4);
-	writeChunk(&chunk, OP_CONSTANT);
-	writeChunk(&chunk, constant2);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constant2, 123);
 
-	writeChunk(&chunk, OP_RETURN);
+	writeChunk(&chunk, OP_RETURN, 123);
 
 	disassembleChunk(&chunk, "test chunk");
 	freeChunk(&chunk);
